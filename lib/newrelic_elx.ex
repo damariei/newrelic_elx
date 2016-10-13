04c19a25945cad763c7ff79fc6c9faeb49df10be
Dmitri Amariei <dmitri.amariei@gmail.com>
2016-10-13 16:01:59 +0200

    check for config settings

--- lib/newrelic_elx.ex
+++ lib/newrelic_elx.ex
@@ -12,11 +12,16 @@ defmodule NewrelicElx do
   Starts staman process, adds listener to send recorded stats to NewRelic
   """
   def start(_type, _args) do
-    NewrelicElx.Sup.start_link()
-    :statman_server.add_subscriber(:statman_aggregator)
-    :newrelic_poller.start_link(fn -> :statman_transformer.poll end)
-    Logger.info "Started NewRelic Monitoring."
+    if !Application.get_env(:newrelic, :application_name) || !Application.get_env(:newrelic, :license_key) do
+      Logger.warn "NewRelic not configured. Stopped Monitoring."
+    else
+      NewrelicElx.Sup.start_link()
+      :statman_server.add_subscriber(:statman_aggregator)
+      :newrelic_poller.start_link(fn -> :statman_transformer.poll end)
+      Logger.info "Started NewRelic Monitoring."
+    end
 
-    {:ok, self}
+      {:ok, self}
+    end
   end
 end
