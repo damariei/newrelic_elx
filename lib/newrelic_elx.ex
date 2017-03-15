e6ba36f127c7817bdd4b0b044444d7bb9de0c2f4
Dmitri Amariei <dmitri.amariei@gmail.com>
2017-03-15 11:28:07 +0100

    update agent version and config

--- lib/newrelic_elx.ex
+++ lib/newrelic_elx.ex
@@ -12,7 +12,7 @@ defmodule NewrelicElx do
   Starts staman process, adds listener to send recorded stats to NewRelic
   """
   def start(_type, _args) do
-    if !Application.get_env(:newrelic, :application_name) || !Application.get_env(:newrelic, :license_key) do
+    if !Application.get_env(:newrelic_elx, :application_name) || !Application.get_env(:newrelic_elx, :license_key) do
       Logger.warn "NewRelic not configured. Stopped Monitoring."
     else
       NewrelicElx.Sup.start_link()
